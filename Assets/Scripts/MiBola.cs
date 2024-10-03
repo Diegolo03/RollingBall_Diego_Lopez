8cd75a7d003cec4890aebf3d4311e4981bd72343
Diegolo03 <diego.atila099@gmail.com>
2024-10-03 21:25:43 +0200

    bola

--- Assets/Scripts/MiBola.cs
+++ Assets/Scripts/MiBola.cs
@@ -5,21 +5,27 @@ using UnityEngine;
 public class MiBola : MonoBehaviour
 {
     Vector3 movimiento;
-    [SerializeField] float velocidad =0f;
+    [SerializeField] float fuerzaSalto =0f, fuerza;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        
         float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical") ;
-        movimiento=new Vector3 (h, 0f, v )* velocidad;
-        transform.Translate(movimiento * Time.deltaTime);
+        float v = Input.GetAxisRaw("Vertical");
+        movimiento = new Vector3(h, 0f, v);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0, fuerzaSalto, 0, ForceMode.Impulse);
+        }
+        rb.AddForce(movimiento*fuerza, ForceMode.Force);
+
+
+        //transform.Translate(movimiento * Time.deltaTime);
     }
 }
